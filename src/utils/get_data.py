db0262919a3e7182d64f0c150ac4e37298be6da2
HamidMalik1 <79519183+HamidMalik1@users.noreply.github.com>
2021-03-03 18:51:46 +0200

    Update get_data.py
    Added CSV path and scheduler

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -1,10 +1,29 @@
 import csv, urllib.request
+import schedule
 
-url = 'http://ilmanet.fi/download.php?orderId=93173&id=127&type=solar&limit=1'
-response = urllib.request.urlopen(url)
-lines = [l.decode('utf-8') for l in response.readlines()]
-cr = csv.reader(lines)
 
-for row in cr:
+def job():
+
+    url = 'http://ilmanet.fi/download.php?orderId=93173&id=127&type=solar&limit=1'
+    response = urllib.request.urlopen(url)
+    lines = [l.decode('utf-8') for l in response.readlines()]
+    cr = csv.reader(lines)
+
+    with open('file4.csv', 'w') as f:
+        for row in cr:
+            for x in row:
+                f.write(str(x) + ',')
+            f.write('\n')
+    return
+schedule.every(1).minutes.do(job)
+#schedule.every().hour.do(job)
+
+while True:
+    schedule.run_pending()
+
+
+#for row in cr:
     #for i in range(len(row)):
-        print(row)
+        #print(row)
+
+
